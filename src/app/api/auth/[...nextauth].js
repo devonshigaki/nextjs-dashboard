c9f6b22374c660284779d72d8c99f41a077dd078
Devon Shigaki <devon.shigaki@freshcredit.com>
2024-09-16 18:07:00 -0700

    pushing to cloud

--- src/app/api/auth/[...nextauth].js
+++ src/app/api/auth/[...nextauth].js
@@ -1,16 +1,3 @@
-// import NextAuth from "next-auth";
-// import Providers from "next-auth/providers";
-
-// export default NextAuth({
-//   providers: [
-//     Providers.Google({
-//       clientId: process.env.GOOGLE_CLIENT_ID,
-//       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-//     }),
-//     // Add other providers if needed
-//   ],
-//   // Add more configuration if needed
-// });
 'use client';
 
 import NextAuth from "next-auth";
--- src/app/lib/actions.js
+++ src/app/lib/actions.js
@@ -1,23 +1,4 @@
-// import { signIn } from '@/auth';
-// import { AuthError } from 'next-auth';
-
-// export async function authenticate(prevState, formData) {
-//   try {
-//     await signIn('credentials', formData);
-//   } catch (error) {
-//     if (error instanceof AuthError) {
-//       switch (error.type) {
-//         case 'CredentialsSignin':
-//           return 'Invalid credentials.';
-//         default:
-//           return 'Something went wrong.';
-//       }
-//     }
-//     throw error;
-//   }
-// }
 import { signIn } from 'next-auth/react';
-// import { signIn } from '@/auth';
 import { AuthError } from 'next-auth';
 // ...
 export async function authenticate(
